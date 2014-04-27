8ce57c4517f6c430a4b7af6c06bac5ba00784ff3
Webb Horn <webbhorn@mit.edu>
2014-04-27 13:47:29 -0700

    Distinguish between read and write faults.

--- src/libdsmu/signals.c
+++ src/libdsmu/signals.c
@@ -1,29 +1,45 @@
 #include <fcntl.h>
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/mman.h>
+#include <ucontext.h>
 
-void pf_sighandler(int sig, struct sigcontext ctx) {
-  printf("called!\n");
-  return;
-}
+#define TRAPNO_PAGE_FAULT 14
+#define REG_ERR 19
 
 static struct sigaction signext;
+
+void pf_sighandler(int sig, siginfo_t *info, ucontext_t *ctx) {
+  if (sig == SIGSEGV) {
+    printf("Got signal %d, fault_loc is %p\n", sig, info->si_addr);
+    printf("%llu\n", ctx->uc_mcontext.gregs[19]);
+    if (ctx->uc_mcontext.gregs[REG_ERR] & 0x2) {
+      printf("WRITE FAULT!\n");
+    } else {
+      printf("READ FAULT!\n");
+    }
+  } else {
+    printf("Got signal %d\n", sig);
+  }
+
+  exit(0);
+}
 
 int main(void) {
   struct sigaction sa;
 
   // Register page fault handler.
-  sa.sa_handler = (void *)pf_sighandler;
+  sa.sa_sigaction = (void *)pf_sighandler;
   sigemptyset(&sa.sa_mask);
-  sa.sa_flags = 0;
+  sa.sa_flags = SA_SIGINFO;
   if (sigaction(SIGSEGV, &sa, &signext) != 0) {
     fprintf(stderr, "sigaction failed\n");
   }
 
   // Setup test memory area.
   int zero_fd = open("/dev/zero", O_RDONLY, 0644);
-  void *p = mmap((void *)0x12340000, 0x4000, (PROT_READ),
+  void *p = mmap((void *)0x12340000, 0x4000, (PROT_NONE),
                  (MAP_ANON|MAP_PRIVATE), zero_fd, 0);
   if (p < 0) {
     fprintf(stderr, "mmap failed.\n");
@@ -33,7 +49,9 @@ int main(void) {
   }
 
   // Trigger a page fault.
-  ((volatile int *)p)[612] = 11;
+  printf("Will try to read %p\n", ((volatile int *)p) + 612);
+  ((volatile int *)p)[612] = 3;
+  //printf("%d\n", ((volatile int *)p)[612]);
 
   printf("we done.\n");
 
