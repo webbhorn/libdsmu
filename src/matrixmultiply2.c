d9e0c98330f0596213b465ab89b5e8fa93f22a97
Tim Donegan <donegan@mit.edu>
2014-05-10 22:24:27 -0400

    wrote matrix mult 2

---
+++ src/matrixmultiply2.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#include "libdsmu.h"
+#include "mem.h"
+
+#define SIZE 1024
+#define SEED 69
+
+typedef int matrix_t [SIZE][SIZE];
+
+int randint() {
+  return rand() % 10;
+}
+
+void print_matrix(matrix_t m) {
+  int i, j;
+  for (i = 0; i < SIZE; i++) {
+    for (j = 0; j < SIZE; j++) {
+      printf(" %d ", m[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+matrix_t A, B;
+
+int main(int argc, char *argv[]) {
+  if (argc < 5) {
+    printf("Usage: main MANAGER_IP MANAGER_PORT id[1|2|...|n] nodes[n]\n");
+    return 1;
+  }
+
+  srand(SEED);
+
+  char *ip = argv[1];
+  int port = atoi(argv[2]);
+  int id = atoi(argv[3]);
+  int n = atoi(argv[4]);
+
+  initlibdsmu(ip, port, 0x12340000, 4096 * 10000);
+
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  double start_ms = (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000;
+
+  matrix_t *C = (matrix_t *) 0x12340000;
+
+  int i, j, k;
+  for (i = 0; i < SIZE; i++) {
+    for (j = 0; j < SIZE; j++) {
+      A[i][j] = randint();
+      B[i][j] = randint();
+    }
+  }
+
+  int factor = SIZE / n + 1;
+
+  for (i = 0; i < SIZE; i++) {
+    if (((i / factor) != (id - 1))) {
+      continue;
+    }
+
+    for (j = 0; j < SIZE; j++) {
+      for (k = 0; k < SIZE; k++) {
+        int temp = A[i][k] * B[k][j];
+        (*C)[i][j] += temp;
+      }
+    }
+  }
+
+  gettimeofday(&tv, NULL);
+  double end_ms = (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000;
+
+  printf("TOTAL TIME (ms): %lf\n", (end_ms - start_ms));
+  printf("done\n");
+
+  printf("sleeping for 10 seconds...\n");
+  sleep(10);
+
+  /*
+  printf("Matrix A\n--------\n");
+  print_matrix(A);
+  printf("Matrix B\n--------\n");
+  print_matrix(B);
+  printf("Matrix C\n--------\n");
+  print_matrix(*C);
+  */
+
+  teardownlibdsmu();
+  return 0;
+}
